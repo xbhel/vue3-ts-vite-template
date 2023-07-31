b67c1b5a0f22e4e277a0b5a0046739bdfb681b60
xbhel <xbhel@outlook.com>
2023-07-31 22:54:03 +0800

    doc(readme.md):修改 todo.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 export default {
-  '*.md,package.json': 'prettier --write',
+  '*.md': 'prettier --write',
+  'package.json': 'prettier --write',
   '*.{js,jsx,ts,tsx,vue}': ['eslint --cache --fix', 'prettier --write'],
   '{!(package)*.json,.!(browserslist)*rc}': 'prettier --write--parser json',
 };
